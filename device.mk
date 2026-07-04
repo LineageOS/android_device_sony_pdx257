c98a3c17b0b71f16f6df4e3bea0c19ae8ec65375
Bruno Martins <bgcngm@gmail.com>
2026-07-04 15:25:13 +0100

    pdx257: Include soong namespaces for NXP keymint3 and weaver HALs
    As of CP2A, these are now behind a soong namespace.
    
    Change-Id: I6cb9b69eba1e2e06c5cfe7224bcad2c96c9e6498

--- device.mk
+++ device.mk
@@ -215,6 +215,10 @@ PRODUCT_PACKAGES += \
     android.hardware.security.keymint3-service.strongbox.nxp \
     android.hardware.weaver-service.nxp
 
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/nxp/keymint/generic \
+    hardware/nxp/weaver/generic
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
 
