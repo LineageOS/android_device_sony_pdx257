9e836098d5a39fc3bb6f6f314d5ac1d06afb8020
LuK1337 <priv.luk@gmail.com>
2026-07-19 19:06:00 +0200

    pdx257: Downgrade to compression.mk
    Change-Id: I3c018e28dd0c92919c5e4909a045966912985363

--- device.mk
+++ device.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
